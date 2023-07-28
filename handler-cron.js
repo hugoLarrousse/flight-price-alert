fdf10681bae5b92ecbcf785a00ce0a669a838173
Hugo Larrousse <hugo.larrousse@gmail.com>
2023-07-28 09:21:24 +0200

    deactivate it

--- handler-cron.js
+++ handler-cron.js
@@ -3,7 +3,7 @@ const index = require('./index');
 module.exports.cron = async () => {
   try {
     console.log('OK')
-    await index.launchScript();
+    // await index.launchScript();
   } catch (error) {
     console.log(error)
   }
